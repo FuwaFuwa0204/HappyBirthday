da14f99a0f0ac4487556a74a60cc92dfe7e91dfd
FuwaFuwa0204 <soyeonmail24@gmail.com>
2023-04-08 18:35:38 +0900

    add image composable

--- app/src/main/java/com/fuwafuwa/happybirthday/MainActivity.kt
+++ app/src/main/java/com/fuwafuwa/happybirthday/MainActivity.kt
@@ -3,15 +3,20 @@ package com.fuwafuwa.happybirthday
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.fuwafuwa.happybirthday.ui.theme.HappyBirthdayTheme
 
@@ -25,22 +30,39 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    BirthdayGreetingWithText(message = "Happy Birthday Sam!", from ="- from Emma")
+                    BirthdayGreetingWithImage(message = stringResource(R.string.happy_birthday_text), from = stringResource(R.string.signature_text))
                 }
             }
         }
     }
 }
 
+@Composable
+fun BirthdayGreetingWithImage(message: String, from: String, modifier: Modifier = Modifier) {
+
+    val image = painterResource(R.drawable.androidparty)
+    Box{Image(
+        painter = image,
+        //image description
+        contentDescription = null,
+        contentScale = ContentScale.Crop
+    )
+        BirthdayGreetingWithText(message = message, from = from)
+    }
+}
 
 @Composable
 fun BirthdayGreetingWithText(message: String, from: String, modifier: Modifier = Modifier) {
 
-    //하위 요소에 modifier 전달달
-   Column(modifier= modifier){
+    //하위 요소에 modifier 전달
+   Column(modifier= modifier.fillMaxSize(),
+       //column이니까 vertical에 arrangement.
+       verticalArrangement = Arrangement.Top,
+       horizontalAlignment = Alignment.CenterHorizontally){
         //text 컴포저블 추가
-        Text(text = message, fontSize = 36.sp)
-        Text(text=from, fontSize = 24.sp)
+       //이 텍스트에만 패딩을 적용하고싶으니까 modifier 다시 정의해서 적용1
+        Text(text = message, fontSize = 36.sp, modifier = Modifier.padding(top = 16.dp))
+        Text(text=from, fontSize = 24.sp, modifier = Modifier.align(alignment = Alignment.End).padding(top = 16.dp,end=16.dp))
     }
 
 }
@@ -49,6 +71,6 @@ fun BirthdayGreetingWithText(message: String, from: String, modifier: Modifier =
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        BirthdayGreetingWithText(message = "Happy Birthday Sam!", from ="- from Emma")
+        BirthdayGreetingWithImage(message = "Happy Birthday Sam!", from ="- from Emma")
     }
 }
